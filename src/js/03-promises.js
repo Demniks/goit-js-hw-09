c01ea55cd40b529ff564d204cf00bf00cb6895fd
Демченко Сергей <Demniks@ukr.net>
2023-02-05 16:36:39 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+const inputDelay = document.querySelector('[name="delay"]');
+const inputStep = document.querySelector('[name="step"]');
+const inputAmount = document.querySelector('[name="amount"]');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let delay = Number(inputDelay.value);
+  const delayStep = Number(inputStep.value);
+  const amount = Number(inputAmount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay).then(onResolve).catch(onReject);
+    delay += delayStep;
   }
+  event.currentTarget.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
+
+function onResolve(value) {
+  Notiflix.Notify.success(value);
+}
+
+function onReject(error) {
+  Notiflix.Notify.failure(error);
 }
